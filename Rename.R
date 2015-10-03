406697b9001ad6d967ac47e3d90f8aa65c0e2fb7
Eric Dose <e@ericdose.com>
2015-10-03 15:16:49 -0500

    Rename.R closer to ready

--- Rename.R
+++ Rename.R
@@ -14,26 +14,40 @@ renameACP <- function (folder="J:/Astro/Images/C11/2015/20150825/Renaming/") {
       df$target[iRow] <- substrings[2]
     }
     
-  # Get exposure-start Julian Date from FITS files.  
+  # Extract multiple metadata from FITS files' headers.  
   require(FITSio)
   get_header_value <- function(header,key) {  # nested function.
     value <- header[which(header==key)+1]
     if (length(value)==0) value <- NA
     value
     }
-  df <- cbind(df, JD_start=NA, Object=NA, Filter=NA, Airmass=NA, FWHM=NA, Exp_secs=NA)
+  df <- cbind(df, Object=NA, JD_start=NA, JD_mid=NA, Filter=NA, Airmass=NA, FWHM=NA, Exp_secs=NA)
   for (iRow in 1:nrow(df)) {  
     fullfilename <- make_safe_path(folder,df$ACP_name[iRow])
-    cat("Opening >",fullfilename,"<\n",sep="")
     fileHandle <- file(description=fullfilename, open="rb")
     header <- parseHdr(readFITSheader(fileHandle))
     close(fileHandle)
-    df$JD_start[iRow] <- get_header_value(header, "JD")
     df$Object[iRow]   <- get_header_value(header, "OBJECT")
+    df$JD_start[iRow] <- get_header_value(header, "JD")
     df$Filter[iRow]   <- get_header_value(header, "FILTER")
     df$Airmass[iRow]  <- get_header_value(header, "AIRMASS")
     df$FWHM[iRow]     <- get_header_value(header, "FWHM")
     df$Exp_secs[iRow] <- get_header_value(header, "EXPTIME")
-  }  
-  return(df)
+  }
+  df$JD_mid <- as.character(as.numeric(df$JD_start) + as.numeric(df$Exp_secs) / (24*3600))
+  cat ("Number of target-Object mismatches (s/b zero) =",sum(df$target != df$Object), "\n")
+  
+  # Construct time-based index for files *within* each target
+  df2 <- df %>% 
+    group_by(Object) %>% 
+    arrange(JD_start) %>% 
+    mutate(one=1) %>% 
+    mutate(index=trimws(as.character(cumsum(one)))) %>%
+    mutate(index=substring(paste("000",index,sep=""),nchar(index)+1,nchar(index)+3)) %>%
+    select(-target,-one)
+
+  # Construct new file names.
+  df2$newName <- paste(df2$Object,"-",df2$index,"-",df2$Filter,sep="")
+  
+  return(df2)
 }
